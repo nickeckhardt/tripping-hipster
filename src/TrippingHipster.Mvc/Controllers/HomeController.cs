15345f183f0f5f832c6e28d8691bf46932f1600f
nickeckhardt <nick_eckhardt@muzak.com>
2013-10-04 14:20:34 -0400

    deltas to test build server integration

---
+++ src/TrippingHipster.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrippingHipster.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public HomeController()
+        {
+            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
